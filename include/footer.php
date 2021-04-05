702cfb6f33e2e66b9e69c61b284fb804fb2bdef7
Vlad <2874787@gmail.com>
2021-04-05 12:29:07 +0700

    doc links

--- include/footer.php
+++ include/footer.php
@@ -8,9 +8,9 @@
 			<p><a href="about.php">О нас</a></p>
 		</div>
 		<div class="footer__doc container">
-			<p><a href="#">Пользовательское соглашение</a></p>
-			<p><a href="#">Политика конфиденциальности</a></p>
-			<p><a href="#">Публичная оферта</a></p>
+			<p><a href="documents.php#terms_of_use">Пользовательское соглашение</a></p>
+			<p><a href="documents.php#privacy_policy">Политика конфиденциальности</a></p>
+			<p><a href="documents.php#public_offer">Публичная оферта</a></p>
 		</div>
 	</div>
 </footer>
