33d15d433831d7cbef3d12fe5d964a2684dde8b0
Vlad <2874787@gmail.com>
2021-06-09 17:44:38 +0700

    main menu animating appearence

--- include/header.php
+++ include/header.php
@@ -21,12 +21,6 @@ require_once 'const.php';
 	</a>
 	<nav class="header__burger-menu">
 		<ul class="header__list">
-	<!-- 		<li>
-				<a href="catalog.php" class="header__link"><b>NEW</b></a>
-			</li>
-			<li>
-				<a href="catalog.php" class="header__link"><b>SALE</b></a>
-			</li> -->
 
 
 			<?php
@@ -34,39 +28,6 @@ require_once 'const.php';
 					echo '<li><a href="catalog.php?cat_id%5B%5D=' . "{$value}" . '" class="header__link">' . "{$key}" . '</a></li>';
 			?>
 			
-<!-- 			<li>
-				<a href="catalog.php?cat_id%5B%5D=131" class="header__link">Серьги</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=264" class="header__link">Браслеты, шнуры</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=138" class="header__link">Колье</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=127" class="header__link">Кольца</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=140" class="header__link">Цепи</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=129" class="header__link">Подвески</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=136" class="header__link">Броши</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=126" class="header__link">Аксессуары</a>
-			</li>
- 			<li>
-				<a href="catalog.php?cat_id%5B%5D=128" class="header__link">Мужчинам</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=130" class="header__link">Религия</a>
-			</li>
-			<li>
-				<a href="catalog.php?cat_id%5B%5D=134" class="header__link">Часы</a>
-			</li> -->
 		</ul>
 	</nav>
 	<nav class="header__main-menu main-menu">
@@ -78,46 +39,7 @@ require_once 'const.php';
 						foreach($aAtrItems['cat_id'] as $key => $value)
 							echo '<li><a href="catalog.php?cat_id%5B%5D=' . "{$value}" . '" class="main-sub-menu__link">' . "{$key}" . '</a></li>';
 					?>
-<!-- 					<li>
-						<a href="catalog.php?cat_id%5B%5D=131" class="main-sub-menu__link">Серьги</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=264" class="main-sub-menu__link">Браслеты, шнуры</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=138" class="main-sub-menu__link">Колье</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=127" class="main-sub-menu__link">Кольца</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=140" class="main-sub-menu__link">Цепи</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=129" class="main-sub-menu__link">Подвески</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=136" class="main-sub-menu__link">Броши</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=126" class="main-sub-menu__link">Аксессуары</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=128" class="main-sub-menu__link">Мужчинам</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=130" class="main-sub-menu__link">Религия</a>
-					</li>
-					<li>
-						<a href="catalog.php?cat_id%5B%5D=134" class="main-sub-menu__link">Часы</a>
-					</li>
-				</ul>
-					<li>
-						<a href="" class="main-menu__link header__link">New</a>
-					</li>
-					<li>
-						<a href="" class="main-menu__link header__link">Sale</a>
-					</li> -->
+
 				</ul> 
 	</nav>
 	<div class="header__search">
